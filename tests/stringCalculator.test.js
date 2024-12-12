483c9c2547d41b82a273f717c18caffadd5f88bb
sabareesh <github@sj.anonaddy.com>
2024-12-12 14:53:40 +0530

    Test case for a single input
    Test case for validation on single input for the add() function

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -1,9 +1,14 @@
 const { add } = require('../src/stringCalculator');
 
-describe('String Calculator', () => {\
+describe('String Calculator', () => {
 
   test('empty string returns 0', () => {
     expect(add("")).toBe(0);
   });
-  
+
+  test('single number returns that number', () => {
+    expect(add("1")).toBe(1);
+    expect(add("5")).toBe(5);
+  });
+
 });
