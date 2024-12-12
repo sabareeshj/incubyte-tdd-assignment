ac4064fa31f44d26239bcc106a6d5b613bce4fc0
sabareesh <github@sj.anonaddy.com>
2024-12-12 15:02:54 +0530

    Test case to handle new line as a valid delimiter

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -21,4 +21,8 @@ describe('String Calculator', () => {
     expect(add("2,3,4,5")).toBe(14);
   });
 
+  test('new line as a delimiter along with commas', () => {
+    expect(add("1\n2,3")).toBe(6);
+  });
+
 });
