9f1f111d4926b3c266b6b1ac4866f94fb6018ff2
sabareesh <github@sj.anonaddy.com>
2024-12-12 15:37:18 +0530

    Add() function handles multi length delimiter now

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,3 +1,21 @@
+const extractDelimiter = (delimiterPart) => {
+    const delimiters = [];
+    const bracketed = delimiterPart.match(/\[([^]+?)\]/g);
+
+    if (bracketed) {
+        bracketed.forEach(b => {
+            delimiters.push(b.slice(1, -1));
+        });
+    } else {
+        delimiters.push(delimiterPart);
+    }
+    return delimiters;
+}
+
+function escapeRegExp(str) {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
 function add(numbers) {
     if (numbers === "") return 0;
 
@@ -7,20 +25,21 @@ function add(numbers) {
     if (numbers.startsWith("//")) {
         const newlineIndex = numbers.indexOf("\n");
         const delimiterPart = numbers.substring(2, newlineIndex);
-        delimiter = delimiterPart;
+        delimiter = extractDelimiter(delimiterPart);
         numbers = numbers.slice(newlineIndex + 1, numbers.length + 1);
         console.log(numbers)
     } else {
         numbers = numbers.replace(/\n/g, ',');
-        delimiter = ',';
+        delimiter = [','];
     }
 
 
-    const nums = numbers.split(delimiter)
+    const regex = new RegExp(delimiter.map(d => escapeRegExp(d)).join("|"), "g");
+    const nums = numbers.split(regex)
         .map(num => parseInt(num, 10))
         .filter(n => !isNaN(n))
         .filter(n => n <= 1000);
-        
+
     const negatives = nums.filter(n => n < 0);
 
     if (negatives.length > 0) {
