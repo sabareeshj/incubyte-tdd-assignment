491023729298efacb924fb9bbe57f0d5ae562000
sabareesh <github@sj.anonaddy.com>
2024-12-12 15:04:41 +0530

    Add function replaces '\n' with ','

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,6 +1,7 @@
 function add (numbers) {
     if (numbers === "") return 0;
 
+    numbers = numbers.replace(/\n/g, ',');
     const nums = numbers.split(",");
     const sum = nums.reduce((total, num) => total + parseInt(num, 10), 0);
     return sum;
