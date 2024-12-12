5b4ed2d245fa7928998d6455047562c301578c72
sabareesh <github@sj.anonaddy.com>
2024-12-12 15:12:45 +0530

    Logic to extract delimiter
    add() function now accepts dyanmic delimter from the input

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,10 +1,22 @@
-function add (numbers) {
+function add(numbers) {
     if (numbers === "") return 0;
 
-    numbers = numbers.replace(/\n/g, ',');
-    const nums = numbers.split(",");
-    const sum = nums.reduce((total, num) => total + parseInt(num, 10), 0);
-    return sum;
+    // default delimiters: comma or newline
+    let delimiter = /[,\n]/;
+
+    if (numbers.startsWith("//")) {
+        const newlineIndex = numbers.indexOf("\n");
+        const delimiterPart = numbers.substring(2, newlineIndex);
+        delimiter = delimiterPart;
+        numbers = numbers.slice(newlineIndex + 1, numbers.length + 1);
+        console.log(numbers)
+    } else {
+        numbers = numbers.replace(/\n/g, ',');
+        delimiter = ',';
+    }
+
+    const nums = numbers.split(delimiter).map(num => parseInt(num, 10));
+    return nums.reduce((total, num) => total + num, 0);
 }
 
 module.exports = {
