4f926a08475ddaa35cbb1d34becf4279ad00a799
sabareesh <github@sj.anonaddy.com>
2024-12-12 14:57:45 +0530

    Test case for comma separated input

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -11,4 +11,9 @@ describe('String Calculator', () => {
     expect(add("5")).toBe(5);
   });
 
+  test('two comma-separated numbers return their sum', () => {
+    expect(add("1,2")).toBe(3);
+    expect(add("2,3")).toBe(5);
+  });
+
 });
