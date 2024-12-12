5037d4f79303999c2f713155a240fe399b091708
sabareesh <github@sj.anonaddy.com>
2024-12-12 15:05:57 +0530

    Test case to handle different delimiters

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -25,4 +25,9 @@ describe('String Calculator', () => {
     expect(add("1\n2,3")).toBe(6);
   });
 
+  test('support custom delimiter', () => {
+    expect(add("//;\n1;2")).toBe(3);
+    expect(add("//|\n2|3|4")).toBe(9);
+  });
+
 });
