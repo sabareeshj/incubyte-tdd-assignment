8dd2e352bd748138489ac241a00cb303ea9a0a44
sabareesh <github@sj.anonaddy.com>
2024-12-12 15:16:28 +0530

    Implemented logic to check for negative numbers
    add() throws excpetion on encountering negative numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -16,6 +16,12 @@ function add(numbers) {
     }
 
     const nums = numbers.split(delimiter).map(num => parseInt(num, 10));
+    const negatives = nums.filter(n => n < 0);
+    
+    if (negatives.length > 0) {
+        throw new Error(`negatives not allowed: ${negatives.join(",")}`);
+    }
+
     return nums.reduce((total, num) => total + num, 0);
 }
 
